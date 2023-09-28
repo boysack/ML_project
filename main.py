98bf476408a2f0e0e42d606d5d3d3b0090444c6c
NICOLO' IACOBONE s317025 <nicolo.iacobone@studenti.polito.it>
2023-09-28 11:43:25 +0200

    edit plot folder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from plots import *
 
 if __name__=="__main__":
   dtr,ltr = load_csv('data/Train.txt')
-  # generate_and_save_features_hists(z_normalization(dtr), ltr, './plots/')
-  # generate_and_save_features_scatter_plots(z_normalization(dtr), ltr, './plots/')
+  generate_and_save_features_hists(z_normalization(dtr), ltr, './plots/histograms/')
+  generate_and_save_features_scatter_plots(z_normalization(dtr), ltr, './plots/scatter_plots/')
 
-  
+
