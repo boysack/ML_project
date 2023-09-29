57949ac5aad11d2ed33c8994ad758a72d838a914
boysack <claudiomacaluso@gmail.com>
2023-09-29 21:18:36 +0200

    main, preprocessing

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ from preprocessing import *
 import numpy as np
 from preprocessing import *
 
+def load_csv(file_name: str) -> tuple:
+  data = []
+  labels = []
+  with open(file_name, 'r') as opened_file:
+    for line in opened_file:
+      sample = [float(i) for i in line.rstrip().split(',')[0:-1]]
+      data.append(sample)
+      labels.append(line.rstrip().split(',')[-1])
+  keys = np.unique(labels)
+  labels = np.array(labels)
+  l = np.zeros(labels.size)
+  for i in range(keys.size):
+    l[labels==keys[i]] = i
+  return np.array(data).T, l
+
 if __name__=="__main__":
-  dtr,ltr = load_csv('data/Train.txt')
-  heatmaps(dtr, ltr)
+  #dtr,ltr = load_csv('data/Train.txt')
+  dtr, ltr = load_csv('iris.csv')
+  
+  """sol_pca = np.load('IRIS_PCA_matrix_m4.npy')
+  sol_lda = np.load('IRIS_LDA_matrix_m2.npy')
+  pca = pca(dtr)
+  pca.process(4)
+  lda = lda(dtr, ltr)
+  lda.process()
+  print(f'PCA error: {(sol_pca-pca.eigenvectors[:,:4]).max()}')
+  print(f'LDA error: {(sol_lda-lda.eigenvectors[:,:2]).max()}') """
+  
+
+  
--- preprocessing.py
+++ preprocessing.py
@@ -25,7 +25,7 @@ class pca(prepocess):
 
     def compute_directions(self):
         C = covariance_matrix(self.original_data)
-        self.eigenvalues, self.eigenvectors = sp.linalg.eigh(C)
+        self.eigenvalues, self.eigenvectors = np.linalg.eigh(C)
         self.eigenvalues = self.eigenvalues[::-1]
         self.eigenvectors = self.eigenvectors[:,::-1]
 
@@ -36,7 +36,7 @@ class pca(prepocess):
             control_value = self.original_data.shape[0]
             if(m<1 and m>control_value):
                 self.m = 1
-            self.projected_data = np.dot(self.eigenvectors.T, self.original_data)
+            self.projected_data = np.dot(self.eigenvectors[:,:self.m].T, self.original_data)
             self.is_preprocessed = True
 
 #binary lda - m = 1
@@ -44,14 +44,14 @@ class lda(prepocess):
     def __init__(self, data, labels) -> None:
         super().__init__(data)
         self.labels = labels
-        self.m = 1
+        self.m = 2
 
     def compute_directions(self):
         data_mean = mean(self.original_data)
         classes = classes_number(self.labels)
         mean_of_classes = compute_mean_of_classes(self.original_data, self.labels)
-        S_b = np.zeros((self.original_data.shape[0], self.original_data.shape[0]))
-        S_w = np.zeros((self.original_data.shape[0], self.original_data.shape[0]))
+        S_b = 0
+        S_w = 0
         for i in range(classes):
             S_b += (self.labels == i).sum() * np.dot(mean_of_classes[i] - data_mean, (mean_of_classes[i] - data_mean).T)
             S_w += np.dot(self.original_data[:, self.labels == i] - mean_of_classes[i], (self.original_data[:, self.labels == i] - mean_of_classes[i]).T)
@@ -66,5 +66,5 @@ class lda(prepocess):
     def process(self):
         if(self.eigenvectors is None):
             self.compute_directions()
-        self.projected_data = np.dot(self.eigenvectors.T, self.original_data)
+        self.projected_data = np.dot(self.eigenvectors[:,:self.m].T, self.original_data)
         self.is_preprocessed = True
